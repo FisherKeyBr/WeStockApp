8a7e23541df81f47dfea3d040f988c3248f1a7f4
Gabriel <gabriel.dev30@gmail.com>
2023-05-08 09:04:48 -0300

    signup page implemented

--- WeStock.App/wwwroot/webapp/src/app/signup/signup.component.ts
+++ WeStock.App/wwwroot/webapp/src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ExceptionService } from '../exception.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  constructor(private userService: UserService, private exceptionService: ExceptionService) {
+
+  }
+
+  formData: any = {
+    username: '',
+    password: '',
+    displayName: ''
+  };
+
+  onRegisterClick() {
+    this.userService.register(this.formData).then(() => {
+      alert('The user was created!');
+    }).catch((err:any) => this.exceptionService.handleError(err));
+  }
 }
---
+++ WeStock.App/wwwroot/webapp/src/app/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  register(formData: any) {
+    const uri = "/api/User";
+    return this.http.post<any>(uri, formData).toPromise();
+  }
+}
